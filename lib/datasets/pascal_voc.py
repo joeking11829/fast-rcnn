de67c2226563882d479dfff20cc72d074306c742
Joe Young <joeking11829@gmail.com>
2015-12-29 10:03:56 +0800

    reset pascal_voc.py

--- lib/datasets/pascal_voc.py
+++ lib/datasets/pascal_voc.py
@@ -26,12 +26,11 @@ class pascal_voc(datasets.imdb):
                             else devkit_path
         self._data_path = os.path.join(self._devkit_path, 'VOC' + self._year)
         self._classes = ('__background__', # always index 0
-                         'person')
-                         #'aeroplane', 'bicycle', 'bird', 'boat',
-                         #'bottle', 'bus', 'car', 'cat', 'chair',
-                         #'cow', 'diningtable', 'dog', 'horse',
-                         #'motorbike', 'person', 'pottedplant',
-                         #'sheep', 'sofa', 'train', 'tvmonitor')
+                         'aeroplane', 'bicycle', 'bird', 'boat',
+                         'bottle', 'bus', 'car', 'cat', 'chair',
+                         'cow', 'diningtable', 'dog', 'horse',
+                         'motorbike', 'person', 'pottedplant',
+                         'sheep', 'sofa', 'train', 'tvmonitor')
         self._class_to_ind = dict(zip(self.classes, xrange(self.num_classes)))
         self._image_ext = '.jpg'
         self._image_index = self._load_image_set_index()
@@ -139,7 +138,7 @@ class pascal_voc(datasets.imdb):
                                                 self.name + '.mat'))
         assert os.path.exists(filename), \
                'Selective search data not found at: {}'.format(filename)
-        raw_data = sio.loadmat(filename)['all_boxes'].ravel()
+        raw_data = sio.loadmat(filename)['boxes'].ravel()
 
         box_list = []
         for i in xrange(raw_data.shape[0]):
